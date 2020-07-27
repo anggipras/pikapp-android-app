2ed42ba4246b61097f1e7caa1d895ddffcecf321
Johanes Steven <bejohen@gmail.com>
2020-07-27 14:00:24 +0700

    1. login-backend integration (login still using phone number) 2. adding ui button 3. adding constants

--- app/src/main/java/com/bejohen/pikapp/models/Model.kt
+++ app/src/main/java/com/bejohen/pikapp/models/Model.kt
@@ -4,25 +4,46 @@ import com.google.gson.annotations.SerializedName
 
 
 data class LoginRequest(
-    @SerializedName("email")
+    @SerializedName("username")
     val email: String?,
     @SerializedName("password")
     val password: String?
 )
 
 data class LoginResponse(
-    val user: String?,
-    val token: String?
+    @SerializedName("err_code")
+    val errCode: String?,
+
+    @SerializedName("err_message")
+    val errMessage: String?,
+
+    val token:String?
 )
 
 data class RegisterRequest(
     @SerializedName("email")
     val email: String?,
+
     @SerializedName("password")
-    val password: String?
+    val password: String?,
+
+    @SerializedName("full_name")
+    val fullName: String?,
+
+    @SerializedName("phone_number")
+    val phoneNumber: String?,
+
+    @SerializedName("birth_day")
+    val birthday: String?,
+
+    @SerializedName("gender")
+    val gender: String?
 )
 
 data class RegisterResponse(
-    val user: String?,
-    val token: String?
+    @SerializedName("err_code")
+    val errCode: String?,
+
+    @SerializedName("err_message")
+    val errMessage: String?
 )
