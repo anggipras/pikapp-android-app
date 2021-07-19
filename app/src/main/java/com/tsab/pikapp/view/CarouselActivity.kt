9462ca492260f7d6cac9a788f95ba2af7bc1cb17
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-07-19 14:07:12 +0700

    Added click functionality to the next button in CarouselActivity

--- app/src/main/java/com/tsab/pikapp/view/CarouselActivity.kt
+++ app/src/main/java/com/tsab/pikapp/view/CarouselActivity.kt
@@ -1,10 +1,7 @@
 package com.tsab.pikapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.tsab.pikapp.R
@@ -12,40 +9,32 @@ import com.tsab.pikapp.view.onboarding.CarouselViewPagerAdapter
 import com.tsab.pikapp.view.onboarding.screens.CarouselOneFragment
 import com.tsab.pikapp.view.onboarding.screens.CarouselThreeFragment
 import com.tsab.pikapp.view.onboarding.screens.CarouselTwoFragment
-import kotlinx.android.synthetic.main.fragment_onboarding_view_pager.*
 
 class CarouselActivity : AppCompatActivity() {
     lateinit var viewPager: ViewPager2
-    lateinit var nextBtn: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_carousel)
 
-      viewPager =  findViewById(R.id.carousel_viewpager)
-      nextBtn = findViewById(R.id.nextBtn)
+        viewPager = findViewById(R.id.carousel_viewpager)
 
+        val onNextPageCallback = { viewPager.currentItem += 1 }
         val fragments: ArrayList<Fragment> = arrayListOf(
-            CarouselOneFragment(),
-            CarouselTwoFragment(),
-            CarouselThreeFragment()
+                CarouselOneFragment.newInstance(onNextPageCallback),
+                CarouselTwoFragment.newInstance(onNextPageCallback),
+                CarouselThreeFragment()
         )
-
-        nextBtn.setOnClickListener {
-            viewPager.currentItem = viewPager.currentItem + 1
-        }
 
         val adapter = CarouselViewPagerAdapter(fragments, this)
         viewPager.adapter = adapter
-
     }
 
-
     override fun onBackPressed() {
-        if(viewPager.currentItem == 0){
+        if (viewPager.currentItem == 0) {
             super.onBackPressed()
-        }else{
+        } else {
             viewPager.currentItem = viewPager.currentItem - 1
         }
     }
-
 }
--- app/src/main/java/com/tsab/pikapp/view/onboarding/screens/CarouselTwoFragment.kt
+++ app/src/main/java/com/tsab/pikapp/view/onboarding/screens/CarouselTwoFragment.kt
@@ -1,25 +1,44 @@
 package com.tsab.pikapp.view.onboarding.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import com.tsab.pikapp.R
+import kotlinx.android.synthetic.main.fragment_carousel_two.*
+import java.io.Serializable
 
 class CarouselTwoFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    companion object {
+        private const val CALLBACK_ON_NEXT = "callback_on_next"
 
+        // When initiating the fragment, use this method.
+        fun newInstance(onNext: () -> Unit) = CarouselTwoFragment().apply {
+            // Put onNext callback function to argument bundle
+            arguments = bundleOf(CALLBACK_ON_NEXT to onNext as Serializable)
         }
     }
 
+    private var onNextCallback: () -> Unit = {}
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_carousel_two, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Attempt to fetch onNext callback function from arguments
+        try {
+            onNextCallback = arguments?.getSerializable(CALLBACK_ON_NEXT) as () -> Unit
+            nextBtn?.setOnClickListener { onNextCallback.invoke() }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
