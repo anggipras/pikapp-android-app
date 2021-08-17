fd230e13fa02af5a3ebc366e9da07f3d97a6f3f8
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-08-18 02:30:37 +0700

    Update category list page

--- app/src/main/java/com/tsab/pikapp/view/menuCategory/CategoryNavigation.kt
+++ app/src/main/java/com/tsab/pikapp/view/menuCategory/CategoryNavigation.kt
@@ -1,12 +1,18 @@
 package com.tsab.pikapp.view.menuCategory
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.tsab.pikapp.R
+import com.tsab.pikapp.viewmodel.categoryMenu.CategoryViewModel
 
 class CategoryNavigation : AppCompatActivity() {
+    private val viewModel: CategoryViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        viewModel.fetchCategoryList()
     }
 }
