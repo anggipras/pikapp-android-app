e2462a6cbe8776b0f64f3654ea75aba250a5266e
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2021-08-25 15:05:31 +0700

    Create omnichannel navigation and empty state screen

---
+++ app/src/main/java/com/tsab/pikapp/view/omni/integration/IntegrationActivity.kt
@@ -0,0 +1,12 @@
+package com.tsab.pikapp.view.omni.integration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tsab.pikapp.R
+
+class IntegrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_integration)
+    }
+}
---
+++ app/src/main/java/com/tsab/pikapp/view/omni/integration/IntegrationEmptyFragment.kt
@@ -0,0 +1,31 @@
+package com.tsab.pikapp.view.omni.integration
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.tsab.pikapp.R
+import com.tsab.pikapp.databinding.FragmentIntegrationEmptyBinding
+
+class IntegrationEmptyFragment : Fragment() {
+    private lateinit var navController: NavController
+    private lateinit var dataBinding: FragmentIntegrationEmptyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_integration_empty, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+    }
+}
