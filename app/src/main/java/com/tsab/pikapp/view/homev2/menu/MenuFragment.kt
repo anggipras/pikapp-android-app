bfb539a31366bd0c3f6f6b4cc41d50713145d15f
adriankurnia <adriankurnia14@gmail.com>
2021-09-08 09:46:19 +0700

    debugging force closed

--- app/src/main/java/com/tsab/pikapp/view/homev2/menu/MenuFragment.kt
+++ app/src/main/java/com/tsab/pikapp/view/homev2/menu/MenuFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -118,9 +119,7 @@ class MenuFragment : Fragment() {
                                 activity?.startActivity(intent)
                             }
                             dataBinding.buttonSort.setOnClickListener {
-                                val intent =
-                                        Intent(activity?.baseContext, CategoryNavigation::class.java)
-                                activity?.startActivity(intent)
+                                Toast.makeText(activity, "clicked", Toast.LENGTH_SHORT).show()
                             }
                         }
                     })
