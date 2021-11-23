220001c4b9772efa367e2aecdefc1d012dd601d0
LAPTOP-EBF7V94O\ANGGIPRASTIANTO <anggiprastianto30@gmail.com>
2021-11-23 16:43:46 +0700

    create menu web view in app

---
+++ app/src/main/java/com/tsab/pikapp/view/homev2/menu/WebMenuActivity.kt
@@ -0,0 +1,30 @@
+package com.tsab.pikapp.view.homev2.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebViewClient
+import com.tsab.pikapp.R
+import kotlinx.android.synthetic.main.activity_web_menu.*
+
+class WebMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_menu)
+        val midStore = intent.getStringExtra("MID")
+        webViewSetup(midStore)
+
+        menuWeb_back.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun webViewSetup(mid: String) {
+        menuWebView.webViewClient = WebViewClient()
+        menuWebView.apply {
+            loadUrl("https://web-dev.pikapp.id/store?mid=${mid}")
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+        }
+    }
+}
