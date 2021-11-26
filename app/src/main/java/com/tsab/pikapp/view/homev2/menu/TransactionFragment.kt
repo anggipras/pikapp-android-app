bf87f580e561d8f40265d664cca85c060bed8f72
LAPTOP-EBF7V94O\ANGGIPRASTIANTO <anggiprastianto30@gmail.com>
2021-11-26 16:55:33 +0700

    hide manual transaction flow

--- app/src/main/java/com/tsab/pikapp/view/homev2/menu/TransactionFragment.kt
+++ app/src/main/java/com/tsab/pikapp/view/homev2/menu/TransactionFragment.kt
@@ -51,17 +51,18 @@ class TransactionFragment : Fragment() {
                 setUpTabs()
             }
 
-            topAppBar.setOnMenuItemClickListener { menuItem ->
-                when (menuItem.itemId) {
-                    R.id.manualTxn -> {
-                        val intent = Intent(activity?.baseContext, ManualTxnActivity::class.java)
-                        activity?.startActivityForResult(intent, 1)
-                        activity?.overridePendingTransition(0, 0)
-                        true
-                    }
-                    else -> false
-                }
-            }
+            /* Hide manual transaction for next sprint */
+//            topAppBar.setOnMenuItemClickListener { menuItem ->
+//                when (menuItem.itemId) {
+//                    R.id.manualTxn -> {
+//                        val intent = Intent(activity?.baseContext, ManualTxnActivity::class.java)
+//                        activity?.startActivityForResult(intent, 1)
+//                        activity?.overridePendingTransition(0, 0)
+//                        true
+//                    }
+//                    else -> false
+//                }
+//            }
 
             swipeRefreshLayout = swipeTransactionMenu
 
